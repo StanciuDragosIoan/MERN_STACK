0e9691aa0a477385af996c57e0a3718e0500839c
Stanciu Dragos Ioan <stanciudragosioan92@gmail.com>
2019-12-28 00:45:26 +0200

    test

---
+++ apps/bits/5_server_db_api_routes_model_plus_backend_validation/course_notes/section_2_express_and_mongoDB_setup/7_connecting_to_mongoDB_with_mongoose/notes.js
@@ -0,0 +1,15 @@
+/*
+    Connecting to mongoDB with mongoose
+
+        copied connection string from clusters->connect->connect ur app 
+        created a 'config' directory in the root
+
+        inside 'config' created a default.json file (this is used by the config 
+        dependency and inside default.json we can crete global vars to use throughout 
+        the app)
+
+        added the connection string as a mongoURI entry in the default.json file
+
+        created a db.js inside the 'config' directory
+
+*/
---
+++ course_notes/Section_3_user_api_routes_and_jwt_authentication/10_request_and_body_validation/notes.js
@@ -0,0 +1,9 @@
+/*
+    Request and body validation
+
+    started working on the registration route for the users.js file
+
+    implemented validation with express-validator and check
+
+    tested request from postman
+*/
